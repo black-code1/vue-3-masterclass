7fd0fd1873c83761b6f699aa8036b3eb6de923dd
black-code1 <legrandmahamat1@gmail.com>
2022-12-30 00:07:12 +0100

    Adding new posts

--- src/pages/ThreadShowPage.vue
+++ src/pages/ThreadShowPage.vue
@@ -3,6 +3,17 @@
     <h1>{{thread.title}}</h1>
 
     <post-list :posts="threadPosts "/>
+
+    <div class="col-full">
+      <form @submit.prevent="addPost">
+        <div class="form-group">
+          <textarea v-model="newPostText" name="" id="" cols="30" rows="10" class="form-input"></textarea>
+        </div>
+        <div class="form-actions">
+          <button class="btn-blue">Submit post</button>
+        </div>
+      </form>
+    </div>
   </div>
 
 </template>
@@ -23,6 +34,7 @@ export default {
     return{
       threads: sourceData.threads,
       posts: sourceData.posts,
+      newPostText: ''
     }
   },
 
@@ -34,6 +46,21 @@ export default {
       return this.posts.filter(post => post.threadId === this.id)
     }
   },
+  methods: {
+    addPost() {
+      const postId = 'gggg' + Math.random()
+      const post = {
+        id: postId,
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: 'rpbB8C6ifrYmNDufMERWfQUoa202'
+      }
+      this.posts.push(post)
+      this.thread.posts.push(postId)
+      this.newPostText = ''
+    }
+  }
 
 }
 </script>
