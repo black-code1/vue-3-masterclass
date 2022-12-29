3fd57864e750fc81cb644579dafbdbab707515f1
black-code1 <legrandmahamat1@gmail.com>
2022-12-29 23:35:17 +0100

    Extract posts to a component

--- src/pages/ThreadShowPage.vue
+++ src/pages/ThreadShowPage.vue
@@ -1,40 +1,18 @@
 <template>
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
-    <div class="post-list">
-      <div class="post" v-for="postId in thread.posts" :key="postId">
-        <div class="user-info">
-          <a href="#" class="user-name">{{userById(postById(postId).userId).name}}</a>
 
-          <a href="#">
-            <img class="avatar-large" :src="userById(postById(postId).userId).avatar" alt="">
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>
-              {{postById(postId).text}}
-            </p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{postById(postId).publishedAt}}
-        </div>
-
-
-      </div>
-    </div>
+    <post-list :posts="threadPosts "/>
   </div>
 
 </template>
 
 <script>
 import sourceData from '@/data.json'
+import PostList from "@/components/PostList.vue";
 export default {
+  name: 'ThreadShowPage',
+  components: {PostList},
   props: {
     id: {
       required: true,
@@ -45,25 +23,18 @@ export default {
     return{
       threads: sourceData.threads,
       posts: sourceData.posts,
-      users: sourceData.users
     }
   },
 
   computed: {
     thread() {
       return this.threads.find(thread => thread.id === this.id) //also available under this.$route.params.id
+    },
+    threadPosts () {
+      return this.posts.filter(post => post.threadId === this.id)
     }
   },
 
-  methods: {
-    postById (postId){
-      return this.posts.find(post => post.id === postId)
-    },
-    userById (userId){
-      return this.users.find(user => user.id === userId)
-    },
-
-  }
 }
 </script>
 
