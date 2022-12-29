b3a2a84a4e4de5ce6661249c28f592fcacd44706
black-code1 <legrandmahamat1@gmail.com>
2022-12-29 20:26:58 +0100

    Extract Router Configuration

--- src/main.js
+++ src/main.js
@@ -1,28 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import PageHome from "@/components/PageHome.vue";
-import {createRouter, createWebHistory} from 'vue-router'
-import PageThreadShow from "@/components/PageThreadShow.vue";
-
-const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: PageHome
-    },
-    {
-        path: '/thread/:id',
-        name: 'ThreadShow',
-        component: PageThreadShow,
-        props: true
-    }
-]
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
-
+import router from "@/router";
 const forumApp = createApp(App)
 forumApp.use(router)
 forumApp.mount('#app')
