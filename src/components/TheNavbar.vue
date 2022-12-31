e0c9e39fdad9eed28799a527293cddb078ca4000
black-code1 <legrandmahamat1@gmail.com>
2022-12-31 11:19:31 +0100

    Fetching the auth user

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -17,11 +17,11 @@
     <ul>
       <li class="navbar-user">
         <a href="#">
-          <img class="avatar-small" src="https://pbs.twimg.com/profile_images/1188775562657091594/5mgkg44t_400x400.jpg" alt="">
+          <img class="avatar-small" :src="authUser.avatar" alt="`${authUser.name} profile picture`">
           <span>
-                        Alex Kyriakidis
-                        <img class="icon-profile" src="../assets/svg/arrow-profile.svg" alt="">
-                    </span>
+              {{authUser.name}}
+              <img class="icon-profile" src="../assets/svg/arrow-profile.svg" alt="">
+          </span>
         </a>
 
         <!-- dropdown menu -->
@@ -63,8 +63,13 @@
 </template>
 
 <script>
+import {mapGetters} from "vuex";
+
 export default {
-  name: "TheNavbar"
+  name: "TheNavbar",
+  computed: {
+    ...mapGetters(['authUser'])
+  }
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,13 @@
 import {createStore} from "vuex";
 import sourceData from '@/data.json'
 export default createStore({
-    state: sourceData,
+    state: {
+        ...sourceData,
+        authId: 'rpbB8C6ifrYmNDufMERWfQUoa202'
+    },
+    getters: {
+        authUser: state => state.users.find(user => user.id === state.authId)
+    },
     actions: {
         createPost (context, post) {
             post.id = 'ggqq' + Math.random()
