ff3dec11a0b0bfd3b3a3247cc207b9dd0c7a0cb3
black-code1 <legrandmahamat1@gmail.com>
2023-01-19 21:04:11 +0100

    Minor Improvements Before Introducing the Cloud Firestore

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -8,7 +8,8 @@
           <img class="avatar-large" :src="userById(post.userId).avatar" alt="">
         </a>
 
-        <p class="desktop-only text-small">107 posts</p>
+        <p class="desktop-only text-small">{{userById(post.userId).postsCount}} posts</p>
+        <p class="desktop-only text-small">{{userById(post.userId).threadsCount}} threads</p>
       </div>
 
       <div class="post-content">
@@ -44,7 +45,7 @@ export default {
   },
   methods: {
     userById (userId){
-      return this.users.find(user => user.id === userId)
+      return this.$store.getters.user(userId)
     },
   }
 }
